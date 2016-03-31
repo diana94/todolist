bf5bf43f7905063b11b02750207a4b23e20a2625
unknown <d.belik@dbelik.inostudio.net>
2016-03-31 13:09:07 +0300

    редизайн функция сортировки обновлена (но с недочетами)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-// $(document).ready(function(){
-//     $scope.$broadcast('rebuild:me');
-// });
+$( document ).ready(function() {
+    superplaceholder({
+        el: input,
+        sentences: [ 'Something to show', 'Another thing to show'],
+        options: {
+            // delay between letters (in milliseconds)
+            letterDelay: 100, // milliseconds
+            // delay between sentences (in milliseconds)
+            sentenceDelay: 1000,
+            // should start on input focus. Set false to autostart
+            startOnFocus: true,
+            // loop through passed sentences
+            loop: false,
+            // Initially shuffle the passed sentences
+            shuffle: false,
+            // Show cursor or not. Shows by default
+            showCursor: true,
+            // String to show as cursor
+            cursor: '|'
+        }
+    });
+});
